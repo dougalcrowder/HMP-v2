e7da2803ad3552040eca1c017f2a69b13bf18f5e
Dougal Crowder <hi@dougal.co>
2022-09-08 23:54:30 +0100

    build

--- src/main.js
+++ src/main.js
@@ -1,11 +1,5 @@
 import './styles/main.css';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
 const env = document.querySelector('body').dataset.env;
 
 // Check that service workers are supported
@@ -19,3 +13,59 @@ if ('serviceWorker' in navigator && env === 'production') {
     }
   });
 }
+
+// const title = document.querySelector('#title');
+// const speed = 0.2;
+// 
+// title.style.transform = 'scale( calc( var(--scrollparallax) * 1 ) )';
+// 
+// window.addEventListener('scroll', function() {
+//   title.style.setProperty('--scrollparallax', (document.body.scrollTop || document.documentElement.scrollTop) * speed);
+// });
+const image1 = document.getElementById('image1');
+window.addEventListener('scroll', () => {
+  let scrollY = window.scrollY/8000;
+  let scale = 1;
+  let amount = scale + scrollY;
+  if (amount >= 1) {
+    image1.style.transform = 'scale('+amount+')';
+  } else {
+    image1.style.transform = 'scale(1)';
+  } 
+});
+
+const image2 = document.getElementById('image2');
+window.addEventListener('scroll', () => {
+  let scrollY2 = window.scrollY/9000;
+  let scale = 1;
+  let amount2 = scale + scrollY2;
+  if (amount2 >= 1) {
+  image2.style.transform = 'scale('+amount2+')';
+  } else {
+    image2.style.transform = 'scale(1)';
+  }
+});
+
+const image3 = document.getElementById('image3');
+window.addEventListener('scroll', () => {
+  let scrollY3 = window.scrollY/8250;
+  let scale = 1;
+  let amount3 = (scale + scrollY3) - 0.1017;
+  if (amount3 >= 1) {
+  image3.style.transform = 'scale('+amount3+')';
+  } else {
+    image3.style.transform = 'scale(1)';
+  }
+});
+
+const image4 = document.getElementById('image4');
+window.addEventListener('scroll', () => {
+  let scrollY4 = window.scrollY/9250;
+  let scale = 1;
+  let amount4 = (scale + scrollY4) - 0.1478;
+  if (amount4 >= 1) {
+  image4.style.transform = 'scale('+amount4+')';
+  } else {
+    amount4.style.transform = 'scale(1)';
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,22 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Marr Sans True Web', ...defaultTheme.fontFamily.sans],
+        plex: ['ibm-plex-sans-condensed', ...defaultTheme.fontFamily.sans],
       },
+      gridTemplateRows: {
+        // Simple 8 row grid
+        '12': 'repeat(12, minmax(0, 1fr))',
+      }
     },
+    colors: {
+      background: '#EEEDED',
+      bookingbg: '#DDD8CD',
+      hmplightgrey: '#6C757D',
+      hmpgrey: '#3F494E',
+      blackish: '#212529',
+      whiter:'#ffffff',
+    }
   },
   variants: {},
   plugins: [require('@tailwindcss/typography')],
